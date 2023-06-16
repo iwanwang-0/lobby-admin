a9bf29a58446f4b4955fbdda3cac102f16987787
martin <me@martin.com>
2023-06-16 18:34:09 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { PieChart, Pie, Cell, Tooltip } from 'recharts';
-
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
 function App() {
   const [data, setData] = useState([]);
@@ -12,17 +10,15 @@ function App() {
     axios.get('https://lobby.cash/api/gaugeRewards/cvx/all/1686182400')
       .then(res => {
         let totalValue = 0;
-        console.log("gaugeRewards: ");
-        console.log(res.data.data.gaugeRewards);
         const tempData = Object.entries(res.data.data.gaugeRewards).map(([key, value]) => {
-          console.log("key: " + key);
-          console.log("totalValue: " + value.totalValue);
           totalValue += value.totalValue;
           return { name: key, totalValue: value.totalValue };
         });
+        // sort the data by totalValue in descending order
+        tempData.sort((a, b) => b.totalValue - a.totalValue);
         const percentageData = tempData.map(obj => ({
           ...obj,
-          percent: (obj.totalValue / totalValue) * 100,
+          percent: Number(((obj.totalValue / totalValue) * 100).toFixed(2)),
         }));
         setData(percentageData);
         setIsLoading(false);
@@ -37,25 +33,55 @@ function App() {
     return <div>Loading...</div>;
   }
 
+  const CustomizedAxisTick = props => {
+    const { x, y, payload } = props;
+    return (
+      <g transform={`translate(${x},${y})`}>
+        <text x={0} y={0} dy={16} textAnchor="end" fill="#666" transform="rotate(-45)">
+          {payload.value}
+        </text>
+      </g>
+    );
+  };
+
   return (
     <div>
-      <PieChart width={800} height={400}>
-        <Pie
-          data={data}
-          cx={120}
-          cy={200}
-          innerRadius={60}
-          outerRadius={80}
-          fill="#8884d8"
-          paddingAngle={5}
-          dataKey="totalValue"
-        >
-          {
-            data.map((entry, index) => <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />)
-          }
-        </Pie>
+      <BarChart
+        width={window.innerWidth - 100}
+        height={800}
+        data={data}
+        margin={{
+          top: 5,
+          right: 30,
+          left: 20,
+          bottom: 200,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="name" height={200} tick={<CustomizedAxisTick />} />
+        <YAxis />
         <Tooltip />
-      </PieChart>
+        <Legend />
+        <Bar dataKey="totalValue" fill="#8884d8" minPointSize={5} />
+      </BarChart>
+      <table style={{ width: '100%', tableLayout: 'fixed' }}>
+        <thead>
+          <tr>
+            <th style={{ padding: '0 5px' }}>Name</th>
+            <th style={{ padding: '0 5px', textAlign: 'right' }}>Total Value</th>
+            <th style={{ padding: '0 5px', textAlign: 'right' }}>Percent (%)</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item, index) => (
+            <tr key={index}>
+              <td style={{ padding: '0 5px' }}>{item.name}</td>
+              <td style={{ padding: '0 5px', textAlign: 'right' }}>{item.totalValue}</td>
+              <td style={{ padding: '0 5px', textAlign: 'right' }}>{item.percent.toFixed(2)}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
